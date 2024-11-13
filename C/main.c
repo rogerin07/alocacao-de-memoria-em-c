c0688c613f1b301dc11ab09bd7ce7c38ec769873
rogerin07 <rogersantosbezerra@gmail.com>
2024-11-12 22:14:36 -0300

    fds c

---
+++ C/main.c
@@ -0,0 +1,18 @@
+int main() {
+    int *x;
+
+    x = malloc(sizeof(int));
+
+    if(x){
+        printf("memoria alocada com sucesso!\n");
+        printf("x = %d\n", *x);
+        *x = 50;
+        printf("x = %d\n", *x);
+
+    }
+    else{
+        printf("erro ao alocar memoria!\n");
+    }
+    return 0;
+
+}
